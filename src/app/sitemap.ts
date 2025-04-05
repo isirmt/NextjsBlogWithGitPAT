1fc803e8ac74bff519bb16c57970cf8f32804328
isirmt <r119306@g.kobe-kosen.ac.jp>
2025-04-05 16:09:34 +0900

    mod: sitemap, robots

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,5 @@
 import { MetadataRoute } from 'next';
-import { getPostsProps, getSeries, getSeriesProps } from '@/lib/getPosts';
-import { getTagsWithLatestDate } from '@/lib/postSorter';
+import { getPostsProps } from '@/lib/getPosts';
 import { lastModified } from '@/static/constant';
 
 export const dynamic = 'force-dynamic';
@@ -10,8 +9,6 @@ const staticPaths = ['/post', '/profile', '/series', '/tags'];
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const posts = await getPostsProps();
-  const seriesNames = await getSeriesProps();
-  const tagsWithDate = getTagsWithLatestDate(posts);
 
   const baseURL = process.env.NEXT_PUBLIC_URL!;
 
@@ -31,30 +28,11 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
   const dynamicPages: MetadataRoute.Sitemap = [];
 
-  for (const series of seriesNames) {
-    const { posts } = await getSeries(series);
-    dynamicPages.push({
-      url: baseURL + '/series/' + series,
-      lastModified: posts[0].data.date ? new Date(posts[0].data.date) : lastModified,
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    });
-  }
-
   posts.forEach((post) => {
     dynamicPages.push({
       url: baseURL + '/post/' + post.slug,
       lastModified: post.data.date ? new Date(post.data.date) : lastModified,
       changeFrequency: 'yearly',
-      priority: 0.8,
-    });
-  });
-
-  tagsWithDate.forEach((tagWithDate) => {
-    dynamicPages.push({
-      url: baseURL + '/tags/' + encodeURIComponent(tagWithDate.tag),
-      lastModified: tagWithDate.latestDate ? new Date(tagWithDate.latestDate) : lastModified,
-      changeFrequency: 'monthly',
       priority: 0.8,
     });
   });
